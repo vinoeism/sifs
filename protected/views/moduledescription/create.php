1d3afa1a0dc56b430bacdb471a6d86b34241e7db
.vinoe.zelur <vinoeism@gmail.com>
2015-07-15 23:31:08 +0530

    Disabled listing of module descriptions - removed the ADMIN side menu item

--- protected/views/moduledescription/create.php
+++ protected/views/moduledescription/create.php
@@ -9,7 +9,7 @@ $this->breadcrumbs=array(
 
 $this->menu=array(
 	//array('label'=>'List Moduledescription', 'url'=>array('index')),
-	array('label'=>'Manage Moduledescription', 'url'=>array('admin')),
+	//array('label'=>'Manage Moduledescription', 'url'=>array('admin')),
 );
 ?>
 
