b2bd314b45877642f8c8d1c8f76d71150bbc5e67
.vinoe.zelur <vinoeism@gmail.com>
2015-07-18 22:36:35 +0530

    1. Rights 2. Generate REFNo logic implemented 3. Due on - now automatically calculated 4. Ability to export to Excel as CSV 5. Print to PDF

--- protected/controllers/InvoiceController.php
+++ protected/controllers/InvoiceController.php
@@ -1,6 +1,6 @@
 <?php
 
-class InvoiceController extends Controller
+class InvoiceController extends RController
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
@@ -15,6 +15,7 @@ class InvoiceController extends Controller
 	{
 		return array(
 			'accessControl', // perform access control for CRUD operations
+			'rights',
 			'postOnly + delete', // we only allow deletion via POST request
 		);
 	}
@@ -24,26 +25,26 @@ class InvoiceController extends Controller
 	 * This method is used by the 'accessControl' filter.
 	 * @return array access control rules
 	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','approve'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+//	public function accessRules()
+//	{
+//		return array(
+//			array('allow',  // allow all users to perform 'index' and 'view' actions
+//				'actions'=>array('index','view'),
+//				'users'=>array('*'),
+//			),
+//			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+//				'actions'=>array('create','update','approve','printPDF'),
+//				'users'=>array('@'),
+//			),
+//			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+//				'actions'=>array('admin','delete'),
+//				'users'=>array('admin'),
+//			),
+//			array('deny',  // deny all users
+//				'users'=>array('*'),
+//			),
+//		);
+//	}
 
 	/**
 	 * Displays a particular model.
@@ -81,8 +82,24 @@ class InvoiceController extends Controller
                         'statusDataProvider' => $statusDataProvider,
 		));
 	}
+        
+        /**
+	 * Generates a new REFNo.
+         * @param Invoice $model instance of the Invoice class
+         * @return string The reference number
+	 */
+        private function generateREFNo($model)
+        {
+                $codePart = Settings::model()->findBySettingKey('companyshortcode').Branch::model()->findByPK($model->branch_id)->branch_code;
+                $yearPart = date('y');
+                $yearPart = $yearPart.($yearPart+1);
+                $numberPart = Settings::model()->findBySettingKey('invoicestart')+Invoice::model()->count();
+                $numberPart = str_pad($numberPart, Settings::model()->findBySettingKey('invoicedecimals'), '0', STR_PAD_LEFT);
+                return $codePart.$yearPart.$numberPart;
+        }
 
-	/**
+
+        /**
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
 	 */
@@ -91,13 +108,24 @@ class InvoiceController extends Controller
 		$model=new Invoice;
                 $model->job_id = $jobId;
                 $model->branch_id = $branchId;
-
+                
+                $model->invoice_date = date('Y-m-d');
+                $model->REFNO = $this->generateREFNo($model);
+                $model->is_active = 1; // By default a newly invoice can be only active
+                
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
 		if(isset($_POST['Invoice']))
 		{
 			$model->attributes=$_POST['Invoice'];
+                        
+                        //calculation of Due on field
+                        $datetime = new DateTime($model->invoice_date);
+                        $interval = Settings::model()->findByPK($model->invoice_terms)->setting_subkey;
+                        $datetime->add(new DateInterval('P'.$interval.'D'));
+                        $model->due_on = $datetime->format('Y-m-d');
+                        
 			if($model->save())
                         {
                                 // Code to copy vouchers with their items in to the invoice automatically
@@ -151,6 +179,12 @@ class InvoiceController extends Controller
 		if(isset($_POST['Invoice']))
 		{
 			$model->attributes=$_POST['Invoice'];
+                        //calculation of Due on field
+                        $datetime = new DateTime($model->invoice_date);
+                        $interval = Settings::model()->findByPK($model->invoice_terms)->setting_subkey;
+                        $datetime->add(new DateInterval('P'.$interval.'D'));
+                        $model->due_on = $datetime->format('Y-m-d');
+                        
 			if($model->save())
 				$this->redirect(array('view','id'=>$model->id));
 		}
@@ -229,6 +263,7 @@ class InvoiceController extends Controller
 //			'dataProvider'=>$dataProvider,
 //		));
                 $this->actionAdmin();
+                
 	}
 
 	/**
@@ -240,12 +275,71 @@ class InvoiceController extends Controller
 		$model->unsetAttributes();  // clear any default values
 		if(isset($_GET['Invoice']))
 			$model->attributes=$_GET['Invoice'];
+                if ($this->isExportRequest()) { //<==== [[ADD THIS BLOCK BEFORE RENDER]]
+                            //set_time_limit(0); //Uncomment to export lage datasets
+                            //Add to the csv a single line of text
+                            //$this->exportCSV(array('POSTS WITH FILTER:'), null, false);
+                            //Add to the csv a single model data with 3 empty rows after the data
+                            //$this->exportCSV($model, array_keys($model->attributeLabels()), false, 3);
+                            //Add to the csv a lot of models from a CDataProvider
+                            $this->exportCSV($model->search(), array('id', 'REFNO', 'invoice_date','party_id','total_tax_1','total_amount'));
+                        }
 
 		$this->render('admin',array(
 			'model'=>$model,
 		));
 	}
+        
+        /**
+	 * Displays a particular model.
+	 * @param integer $id the ID of the model to be displayed
+	 */
+	public function actionPrintPDF($id)
+	{
+                # You can easily override default constructor's params
+                $mPDF1 = Yii::app()->ePdf->mpdf();
 
+                $itemsDataProvider= new CActiveDataProvider('Moduledescription',
+                        array(
+                            'criteria'=>array(
+                                'condition'=>'invoice_id=:invoiceid AND isActive=1',
+                                'params'=>array(':invoiceid'=>  $id),
+                            ),
+                            'pagination'=>array(
+                                'pageSize'=>15,
+                            )
+                        )
+                    );
+                     
+                $statusDataProvider = new CActiveDataProvider('Modulestatus',
+                            array(
+                                'criteria'=>array(
+                                    'condition'=>'invoice_id=:invoiceid',
+                                    'params'=>array(':invoiceid'=>  $id),
+                                ),
+                                'pagination'=>array(
+                                    'pageSize'=>15,
+                                )
+                            )
+                        );
+                # Load a stylesheet
+                $stylesheet = file_get_contents(Yii::getPathOfAlias('webroot.css') . '/main.css');
+                $mPDF1->WriteHTML($stylesheet, 1);
+
+                $stylesheet = file_get_contents(Yii::getPathOfAlias('webroot.css') . '/screen.css');
+                $mPDF1->WriteHTML($stylesheet, 1);
+                
+                $mPDF1->debug = true;
+                $mPDF1->WriteHTML($this->renderPartial('stub',array(
+                            'model'=>$this->loadModel($id),
+                            'itemsDataProvider' => $itemsDataProvider,
+                            'statusDataProvider' => $statusDataProvider,
+                            ),true));
+
+                $mPDF1->Output();
+                exit;
+            
+	}
 	/**
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
@@ -273,4 +367,17 @@ class InvoiceController extends Controller
 			Yii::app()->end();
 		}
 	}
+        
+        /**
+	 * Performs the export to excel functionality .
+	 * @param none
+	 */
+        public function behaviors() {
+            return array(
+                'exportableGrid' => array(
+                    'class' => 'application.components.ExportableGridBehavior',
+                    'filename' => 'invoices.csv',
+                    'csvDelimiter' => ',', //i.e. Excel friendly csv delimiter
+                    ));
+        }
 }
