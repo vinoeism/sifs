6925a857a2addecb48c47a035de0f316b97d5af8
.vinoe.zelur <vinoeism@gmail.com>
2016-12-05 18:09:53 +0530

    Changed the company name

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Seaindia Freight System Prvt LTD',
+	'name'=>'Righteous Global Logistics Pvt LTD',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -132,6 +132,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'vinothraja@seaindia.co.in',
+		'adminEmail'=>'vinothraja@righteous.in',
 	),
 );
