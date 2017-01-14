f817e93e09529363b7c370dd15504ccb817a0637
.vinoe.zelur <vinoeism@gmail.com>
2017-01-14 23:42:28 +0530

    - modified design

--- protected/views/workorder/_form.php
+++ protected/views/workorder/_form.php
@@ -17,47 +17,77 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'wo_date'); ?>
-		<?php echo $form->textField($model,'wo_date'); ?>
+                <?php
+                    $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+                        'model' => $model,
+                        'attribute' => 'wo_date',
+                        'options' => array(
+                            'showOn' => 'both', // also opens with a button
+                            'dateFormat' => 'yy-mm-dd',
+                        ),
+                        'htmlOptions' => array(
+                            'size' => '12', // textField size
+                            'maxlength' => '12', // textField maxlength
+                            'value'=>CTimestamp::formatDate('Y-m-d'),
+                        ),
+                    ));
+                ?>
 		<?php echo $form->error($model,'wo_date'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'job_id'); ?>
-		<?php echo $form->textField($model,'job_id'); ?>
-		<?php echo $form->error($model,'job_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'trip_id'); ?>
-		<?php echo $form->textField($model,'trip_id'); ?>
-		<?php echo $form->error($model,'trip_id'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->labelEx($model,'transporter_id'); ?>
-		<?php echo $form->textField($model,'transporter_id'); ?>
+		<?php echo $form->dropDownList($model,'transporter_id', $model->getTransporterOptions(), array('prompt'=>'-- Select Transporter --',)); ?>
 		<?php echo $form->error($model,'transporter_id'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'trip_type'); ?>
-		<?php echo $form->textField($model,'trip_type',array('size'=>60,'maxlength'=>200)); ?>
+		<?php echo $form->dropDownList($model,'trip_type', $model->getTripTypes(), array('prompt'=>'-- Select Trip Type --',)); ?>
 		<?php echo $form->error($model,'trip_type'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'trip_date_start'); ?>
-		<?php echo $form->textField($model,'trip_date_start'); ?>
+                <?php
+                    $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+                        'model' => $model,
+                        'attribute' => 'trip_date_start',
+                        'options' => array(
+                            'showOn' => 'both', // also opens with a button
+                            'dateFormat' => 'yy-mm-dd',
+                        ),
+                        'htmlOptions' => array(
+                            'size' => '12', // textField size
+                            'maxlength' => '12', // textField maxlength
+
+                        ),
+                    ));
+                ?>
 		<?php echo $form->error($model,'trip_date_start'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'trip_date_end'); ?>
-		<?php echo $form->textField($model,'trip_date_end'); ?>
+                <?php
+                    $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+                        'model' => $model,
+                        'attribute' => 'trip_date_end',
+                        'options' => array(
+                            'showOn' => 'both', // also opens with a button
+                            'dateFormat' => 'yy-mm-dd',
+                        ),
+                        'htmlOptions' => array(
+                            'size' => '12', // textField size
+                            'maxlength' => '12', // textField maxlength
+
+                        ),
+                    ));
+                ?>
 		<?php echo $form->error($model,'trip_date_end'); ?>
 	</div>
 
-	<div class="row">
+<!--	<div class="row">
 		<?php echo $form->labelEx($model,'in_time'); ?>
 		<?php echo $form->textField($model,'in_time'); ?>
 		<?php echo $form->error($model,'in_time'); ?>
@@ -67,55 +97,21 @@
 		<?php echo $form->labelEx($model,'out_time'); ?>
 		<?php echo $form->textField($model,'out_time'); ?>
 		<?php echo $form->error($model,'out_time'); ?>
-	</div>
+	</div>-->
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'from_location'); ?>
-		<?php echo $form->textField($model,'from_location',array('size'=>60,'maxlength'=>500)); ?>
+		<?php echo $form->textArea($model,'from_location',array('size'=>300,'maxlength'=>500)); ?>
 		<?php echo $form->error($model,'from_location'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'from_location_id'); ?>
-		<?php echo $form->textField($model,'from_location_id'); ?>
-		<?php echo $form->error($model,'from_location_id'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->labelEx($model,'to_location'); ?>
-		<?php echo $form->textField($model,'to_location',array('size'=>60,'maxlength'=>500)); ?>
+		<?php echo $form->textArea($model,'to_location',array('size'=>300,'maxlength'=>500)); ?>
 		<?php echo $form->error($model,'to_location'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'to_location_id'); ?>
-		<?php echo $form->textField($model,'to_location_id'); ?>
-		<?php echo $form->error($model,'to_location_id'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'created_by'); ?>
-		<?php echo $form->textField($model,'created_by'); ?>
-		<?php echo $form->error($model,'created_by'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'created_on'); ?>
-		<?php echo $form->textField($model,'created_on'); ?>
-		<?php echo $form->error($model,'created_on'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'updated_by'); ?>
-		<?php echo $form->textField($model,'updated_by'); ?>
-		<?php echo $form->error($model,'updated_by'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'updated_on'); ?>
-		<?php echo $form->textField($model,'updated_on'); ?>
-		<?php echo $form->error($model,'updated_on'); ?>
-	</div>
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
