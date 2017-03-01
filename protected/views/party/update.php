62494f6a1851c354c5fd77565080dadd5a5a77ec
.vinoe.zelur <vinoeism@gmail.com>
2017-03-01 19:57:29 +0530

    Update code to remove the side menu and display party name

--- protected/views/party/update.php
+++ protected/views/party/update.php
@@ -4,18 +4,18 @@
 
 $this->breadcrumbs=array(
 	'Parties'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
+	$model->party_name=>array('view','id'=>$model->id),
 	'Update',
 );
 
-$this->menu=array(
-	//array('label'=>'List Party', 'url'=>array('index')),
-	array('label'=>'Create Party', 'url'=>array('create')),
-	array('label'=>'View Party', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Party', 'url'=>array('admin')),
-);
+//$this->menu=array(
+//	//array('label'=>'List Party', 'url'=>array('index')),
+//	array('label'=>'Create Party', 'url'=>array('create')),
+//	array('label'=>'View Party', 'url'=>array('view', 'id'=>$model->id)),
+//	array('label'=>'Manage Party', 'url'=>array('admin')),
+//);
 ?>
 
-<h1>Update Party <?php echo $model->id; ?></h1>
+<h1>Update <?php echo $model->party_name; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
