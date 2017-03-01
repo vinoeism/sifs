ebe1ff18f4fcd91104a6aa40bda588756f014dc9
.vinoe.zelur <vinoeism@gmail.com>
2017-03-01 20:10:41 +0530

    Removed unnecessary menus Reaaranged the menus Removed the advanced search - irrelevant

--- protected/views/party/admin.php
+++ protected/views/party/admin.php
@@ -14,53 +14,55 @@ $this->menu=array(
 	array('label'=>'Create Party', 'url'=>array('create')),
 );
 
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#party-grid').yiiGridView('update', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
+//Yii::app()->clientScript->registerScript('search', "
+//$('.search-button').click(function(){
+//	$('.search-form').toggle();
+//	return false;
+//});
+//$('.search-form form').submit(function(){
+//	$('#party-grid').yiiGridView('update', {
+//		data: $(this).serialize()
+//	});
+//	return false;
+//});
+//");
 ?>
 
 <h1>Parties</h1>
 
-<p>
+<!--<p>
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
 or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
+</p>-->
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+<?php //echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<!--<div class="search-form" style="display:none">
+<?php // $this->renderPartial('_search',array(
+//	'model'=>$model,
+//)); ?>
+</div> search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'party-grid',
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
-		'short_code',
 		'party_name',
+		'short_code',
                 array(
                     'name'=>'party_type',
                     'filter'=>$model->getPartyTypes(),
                 ),
-                array(
-                    'name'=>'is_blacklisted',
-                    'filter'=>array('1'=>'Yes','0'=>'No'),
-                    'value'=>'$data->is_blacklisted?"Yes":"No"',
-                ),
-		//'is_blacklisted',
 		'pan_no',
 		'service_tax_no',
+            	array(
+                    'name'=>'is_blacklisted',
+                    'type'=>'raw',
+                    'filter'=>$model->is_blacklisted,
+                    'htmlOptions'=>array('style' => 'text-align: center;'),
+                    'value'=>'($data->is_blacklisted==1)?CHtml::image(\'images/active.png\',\'Yes\', array(\'height\'=>\'14px\')):CHtml::image(\'images/inactive.png\',\'Yes\', array(\'height\'=>\'14px\'))',   
+                ),
+		//'is_blacklisted',
 		array(
                     'name'=>'credit_days',
                     'filter'=>$model->getTermsOfPaymentTypes(),
