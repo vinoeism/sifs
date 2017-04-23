0ec9915052f4a3a75501a7362601be758c643024
.vinoe.zelur <vinoeism@gmail.com>
2017-04-23 17:23:15 +0530

    Added Event handling for Jobs

--- protected/controllers/SettingsController.php
+++ protected/controllers/SettingsController.php
@@ -80,6 +80,28 @@ class SettingsController extends RController
 		));
 	}
 
+        /**
+	 * Creates a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionCreateSetting($type)
+	{
+		$model=new Settings;
+                $model->setting_key = $type;
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model);
+
+		if(isset($_POST['Settings']))
+		{
+			$model->attributes=$_POST['Settings'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model->id));
+		}
+
+		$this->render('create',array(
+			'model'=>$model,
+		));
+	}
 	/**
 	 * Updates a particular model.
 	 * If update is successful, the browser will be redirected to the 'view' page.
