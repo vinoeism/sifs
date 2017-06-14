e4241aea1d3a176e0ada2eeef48b264e0c6cebfb
.vinoe.zelur <vinoeism@gmail.com>
2017-06-14 21:42:18 +0530

    Added menu items to create new setting - job expense, branch expense Added filter menu for setting key

--- protected/views/settings/admin.php
+++ protected/views/settings/admin.php
@@ -16,9 +16,13 @@ $this->menu=array(
 );
 $this->sidemenu = array(
 //            array('label'=>'Update Cargo details', 'url'=>array('update', 'id'=>$model->id, 'formName'=>'CARGO')),    
-            array('label'=>'Add Job Expense', 'url'=>array('create', 'jobID'=>$model->id, 'formName'=>'CONTR')),            
-            array('label'=>'Add Branch Expense', 'url'=>array('voucher/create', 'jobID'=>$model->id, 'branchID'=>$model->branch_id)),
-            array('label'=>'Add Job Item', 'url'=>array('invoice/create', 'jobID'=>$model->id, 'branchID'=>$model->branch_id)),
+            array('label'=>'Add Job Expense', 'url'=>array('create', 'type'=>'jobexpense')),            
+            array('label'=>'Add Branch Expense', 'url'=>array('create', 'type'=>'branchexpense')),            
+            array('label'=>'Add Trip type', 'url'=>array('create', 'type'=>'triptype', 'subtype'=>'vehicle')),            
+            array('label'=>'Add Vehicle type', 'url'=>array('create', 'type'=>'vehicle', 'subtype'=>'type')),            
+
+            //array('label'=>'Add Branch Expense', 'url'=>array('voucher/create', 'jobID'=>$model->id, 'branchID'=>$model->branch_id)),
+            //array('label'=>'Add Job Item', 'url'=>array('invoice/create', 'jobID'=>$model->id, 'branchID'=>$model->branch_id)),
     );
 Yii::app()->clientScript->registerScript('search', "
 $('.search-button').click(function(){
@@ -54,7 +58,12 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'filter'=>$model,
 	'columns'=>array(
 		'id',
-		'setting_key',
+                array(
+                    'name' => 'setting_key',
+                    'filter'=>CHtml::listData(Settings::model()->findAll(array('order'=>'setting_key')),'setting_key','setting_key'),                    
+                    'value' => 'isset($data->setting_key)?$data->setting_key:"  -  "',
+                ),            
+//		'setting_key',
 		'setting_subkey',
 		'setting_value',
 		array(
